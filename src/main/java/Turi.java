9d1b5b44d140b17d3b6085a616c8eef8fb77fa53
Giacomo <giacomo.venturini2@studio.unibo.it>
2018-04-26 11:27:01 +0200

    fixed class as requested ;)

---
+++ src/main/java/Turi.java
@@ -0,0 +1,8 @@
+public class Turi {
+
+    private final String meaningfulOutput = "Soz now it's perfect";
+
+    public void meanigfulMethod(){
+        System.out.println(this.meaningfulOutput);
+    }
+}
