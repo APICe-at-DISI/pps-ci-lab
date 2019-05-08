2457f76f876390a9ac3239d8862b7393990489e4
Emiliano Ciavatta <emiliano.ciavatta@studio.unibo.it>
2019-05-08 13:42:23 +0200

    Add build gradle file. Update gitignore

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    application
+}
+
+application {
+    mainClassName = "MainEmiliano"
+}
+
+dependencies {
+    // testCompile("junit:junit:4.12")
+}
+
+repositories {
+    jcenter()
+}
