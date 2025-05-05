37b09b6e3c07df472074cb7e42b630e296ba0e00
Jiahao Guo <jiahao.guo@studio.unibo.it>
2025-05-05 11:35:18 +0200

    ex4

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    java
+}
+repositories { // Where to search for dependencies
+    mavenCentral()
+}
+dependencies {
+// Suppressions for SpotBugs
+   // compileOnly("com.github.spotbugs:spotbugs-annotations:4.9.3")
+    //implementation("org.scala-lang:scala3-library_3:3.6.4")
+}
---
+++ src/main/java/Example.java
@@ -0,0 +1,18 @@
+public class Example {
+    private final int a;
+
+    public Example(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+}
+
+class exampleTest {
+    public static void main(String[] args) {
+        Example example = new Example(8);
+        System.out.println(example);
+    }
+}
