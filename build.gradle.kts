b3e2e4de65353cbce7298ce7edca9f9a3d050a53
Jiahao Guo <jiahao.guo@studio.unibo.it>
2025-05-05 11:45:29 +0200

    comfigured yaml

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,6 @@ repositories { // Where to search for dependencies
 }
 dependencies {
 // Suppressions for SpotBugs
-   // compileOnly("com.github.spotbugs:spotbugs-annotations:4.9.3")
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.9.3")
     //implementation("org.scala-lang:scala3-library_3:3.6.4")
 }
